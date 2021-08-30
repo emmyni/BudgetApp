dd63296d4b4764ecf2c835e3ded35b8d905aefb1
Emmy Ni <emmyni220@gmail.com>
2021-08-30 07:29:45 -0400

    Continue

--- app/src/main/java/com/example/budgetapp/Entertainment.java
+++ app/src/main/java/com/example/budgetapp/Entertainment.java
@@ -3,12 +3,62 @@ package com.example.budgetapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Entertainment extends AppCompatActivity {
+    Button mButton;
+    EditText mEditSubscriptions;
+    EditText mEditShopping;
+    EditText mEditActivities;
+    EditText mEditOther;
+
+    private Double subscriptions;
+    private Double shopping;
+    private Double activities;
+    private Double other;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entertainment);
+
+        mButton = (Button)findViewById(R.id.button);
+        mEditSubscriptions   = (EditText)findViewById(R.id.editSubscriptions);
+        mEditShopping   = (EditText)findViewById(R.id.editShopping);
+        mEditActivities   = (EditText)findViewById(R.id.editActivities);
+        mEditOther   = (EditText)findViewById(R.id.editOther);
+
+        mButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+
+                        String strSubscriptions = mEditSubscriptions.getText().toString();
+                        String strShopping = mEditShopping.getText().toString();
+                        String strActivities = mEditActivities.getText().toString();
+                        String strOther = mEditOther.getText().toString();
+
+                        if (!"".equals(strSubscriptions)){
+                            subscriptions = Double.parseDouble(strSubscriptions);
+                        }
+                        if (!"".equals(strShopping)){
+                            shopping = Double.parseDouble(strShopping);
+                        }
+                        if (!"".equals(strActivities)){
+                            activities = Double.parseDouble(strActivities);
+                        }
+                        if (!"".equals(strOther)){
+                            other = Double.parseDouble(strOther);
+                        }
+                        Log.v("EditText subscriptions", "subscriptions " + subscriptions);
+                        Log.v("EditText shopping", "shopping " + shopping);
+                        Log.v("EditText activities", "activities " + activities);
+                        Log.v("EditText other", "other " + other);
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/budgetapp/Transportation.java
+++ app/src/main/java/com/example/budgetapp/Transportation.java
@@ -3,12 +3,63 @@ package com.example.budgetapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Transportation extends AppCompatActivity {
+
+    Button mButton;
+    EditText mEditGas;
+    EditText mEditMaintenance;
+    EditText mEditPublicTransport;
+    EditText mEditOther;
+
+    private Double gas;
+    private Double maintenace;
+    private Double publicTransport;
+    private Double other;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transportation);
+
+        mButton = (Button)findViewById(R.id.button);
+        mEditGas   = (EditText)findViewById(R.id.editGas);
+        mEditMaintenance   = (EditText)findViewById(R.id.editMaintenance);
+        mEditPublicTransport   = (EditText)findViewById(R.id.editPublicTransport);
+        mEditOther   = (EditText)findViewById(R.id.editOther);
+
+        mButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+
+                        String strGas = mEditGas.getText().toString();
+                        String strMaintenance = mEditMaintenance.getText().toString();
+                        String strPublicTransport = mEditPublicTransport.getText().toString();
+                        String strOther = mEditOther.getText().toString();
+
+                        if (!"".equals(strGas)){
+                            gas = Double.parseDouble(strGas);
+                        }
+                        if (!"".equals(strMaintenance)){
+                            maintenace = Double.parseDouble(strMaintenance);
+                        }
+                        if (!"".equals(strPublicTransport)){
+                            publicTransport = Double.parseDouble(strPublicTransport);
+                        }
+                        if (!"".equals(strOther)){
+                            other = Double.parseDouble(strOther);
+                        }
+                        Log.v("EditText gas", "gas " + gas);
+                        Log.v("EditText maintenace", "maintenace " + maintenace);
+                        Log.v("EditText pTransport", "publicTransport " + publicTransport);
+                        Log.v("EditText other", "other " + other);
+                    }
+                });
     }
 }
