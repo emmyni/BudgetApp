5b1e506c942c57b98919d6473d66b68c7e612121
Emmy Ni <emmyni220@gmail.com>
2021-09-12 16:34:01 -0400

    Start firebase

--- app/src/main/java/com/example/budgetapp/MainActivity.java
+++ app/src/main/java/com/example/budgetapp/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-//    ArrayList<Plan> plans;
     Button mButton;
     Context mContext;
 
