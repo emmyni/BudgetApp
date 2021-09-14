93a7f3645631fab304561a8bdc2da12ee1ecec19
Emmy Ni <emmyni220@gmail.com>
2021-09-13 22:56:15 -0400

    Add registration functionality

--- app/src/main/java/com/example/budgetapp/LoginFragment.java
+++ app/src/main/java/com/example/budgetapp/LoginFragment.java
@@ -1,5 +1,7 @@
 package com.example.budgetapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +9,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
-
+    Button mButton;
+    Context mContext;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -24,7 +28,21 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        mContext = getContext();
+
+        mButton = (Button)view.findViewById(R.id.btn_login);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent activity2Intent = new Intent(mContext, CalendarPage.class);
+                mContext.startActivity(activity2Intent);
+            }
+
+        });
+        return view;
     }
 
 }
--- app/src/main/java/com/example/budgetapp/RegisterFragment.java
+++ app/src/main/java/com/example/budgetapp/RegisterFragment.java
@@ -4,18 +4,34 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFragment extends Fragment {
+    Button mButton;
+    Context mContext;
+    EditText inputEmail, inputPassword, inputConfirmPassword;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -27,7 +43,58 @@ public class RegisterFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        inputEmail = (EditText)view.findViewById(R.id.et_email);
+        inputPassword = (EditText)view.findViewById(R.id.et_password);
+        inputConfirmPassword = (EditText)view.findViewById(R.id.et_repassword);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        mContext = getContext();
+
+        mButton = (Button)view.findViewById(R.id.btn_register);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                PerformAuth();
+            }
+
+        });
+        return view;
+    }
+
+    private void PerformAuth() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        String confirmPassword = inputConfirmPassword.getText().toString();
+
+        if (!email.matches(emailPattern)) {
+            inputEmail.setError("Enter correct email");
+        } else if (password.isEmpty() || password.length() < 6) {
+            inputPassword.setError("Enter proper password");
+        } else if (!password.equals(confirmPassword)) {
+            inputConfirmPassword.setError("Passwords don't match");
+        } else {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        SendUserToNextActivity();
+                        Toast.makeText(mContext, "Successful Registration", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(mContext, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void SendUserToNextActivity() {
+        Intent intent = new Intent(mContext, CalendarPage.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_CLEAR_TASK|intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(intent);
     }
 
 }
