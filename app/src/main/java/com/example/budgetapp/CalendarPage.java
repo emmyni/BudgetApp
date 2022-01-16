143acef0f4d7f3bec38e6996c5bb83f62d78bd8e
Emmy Ni <emmyni220@gmail.com>
2022-01-15 20:17:06 -0500

    Fix warnings

--- app/src/main/java/com/example/budgetapp/CalendarPage.java
+++ app/src/main/java/com/example/budgetapp/CalendarPage.java
@@ -1,8 +1,6 @@
 package com.example.budgetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
